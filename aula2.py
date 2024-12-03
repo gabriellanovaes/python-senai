0756edb987190cf32852cfd37e670d55ae01c7c1
Gabriella Novaes <ga.novaesb@gmail.com>
2024-12-03 09:41:06 -0300

    Add files via upload

---
+++ aula2.py
@@ -0,0 +1,10 @@
+print("bom dia, quero minha casinha")
+nome_usuario = "Gabriella"  # tipo -> str = string
+idade_usuario = 17 # tipo -> int = interger
+altura_usuario = 1.53 # tipo -> float = ponto flutuante
+maior_idade = False # tipo -> bol - boolean
+
+print("Seja bem-vinda", nome_usuario)
+print("Sua idade é",idade_usuario,"anos")
+print("Você tem",altura_usuario, "anos")
+print("Você é maior de idade?" ,maior_idade)
